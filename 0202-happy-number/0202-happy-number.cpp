fb3ae716d8ba43f9d16fe3153e1a4184c027a4d7
Aditya-566 <147388574+Aditya-566@users.noreply.github.com>
2026-02-17 16:37:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.47%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if (n == 1) return true;
+        if (n == 4) return false;   // cycle detection
+        
+        int sum = 0;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        
+        return isHappy(sum);   // return recursive result
+    }
+};
