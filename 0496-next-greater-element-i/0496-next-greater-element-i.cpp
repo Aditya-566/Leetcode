ef7b9727c90ce89a084c11f56bfe2305217691a5
Aditya Sharma <147388574+Aditya-566@users.noreply.github.com>
2026-09-23 20:28:27 +0530

    Time: 4 ms (23.07%), Space: 12.4 MB (81.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,27 +1,51 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        for(int i=0;i<nums1.size();i++){
-            int j=0;
-            while(j<nums2.size() && nums1[i]!=nums2[j]){
-                j++;
+        // vector<int> ans;
+        // for(int i=0;i<nums1.size();i++){
+        //     int j=0;
+        //     while(j<nums2.size() && nums1[i]!=nums2[j]){
+        //         j++;
 
+        //     }
+        //     int k=j+1;
+        //     bool found=false;
+        //     while(k<nums2.size()){
+        //         if(nums2[k]>nums1[i]){
+        //             ans.push_back(nums2[k]);
+        //             found=true;
+        //             break;
+        //         }
+        //         k++;
+        //     }
+        //     if(found==false){
+        //         ans.push_back(-1);
+        //     }
+        // }
+        // return ans;
+
+        int n = nums2.size();
+        vector<int> nge(n, -1);
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
             }
-            int k=j+1;
-            bool found=false;
-            while(k<nums2.size()){
-                if(nums2[k]>nums1[i]){
-                    ans.push_back(nums2[k]);
-                    found=true;
+            if(!st.empty()) {
+                nge[i] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(int i = 0; i < nums1.size(); i++) {
+            for(int j = 0; j < nums2.size(); j++) {
+                if(nums1[i] == nums2[j]) {
+                    ans.push_back(nge[j]);
                     break;
                 }
-                k++;
-            }
-            if(found==false){
-                ans.push_back(-1);
             }
         }
+
         return ans;
     }
 };
